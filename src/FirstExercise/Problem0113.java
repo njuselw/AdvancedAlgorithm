23e42556641b770e4733bf79d3bb849ebced954a
njuselw <2463909185@qq.com>
2021-01-12 19:32:25 +0800

    AC: 1-15

--- src/FirstExercise/Problem0113.java
+++ src/FirstExercise/Problem0113.java
@@ -1,4 +1,19 @@
 package FirstExercise;
 
 public class Problem0113 {
+    /*
+    和为定值的子数组
+    Description
+    Given an array A of size N, find all combination of four elements in the array whose sum is equal to a given value K.
+    For example, if the given array is {10, 2, 3, 4, 5, 9, 7, 8} and K = 23, one of the quadruple is “3 5 7 8” (3 + 5 + 7 + 8 = 23).
+
+    Input
+    The first line of input contains an integer T denoting the no of test cases.
+    Then T test cases follow. Each test case contains two lines. The first line of input contains two integers N and K.
+    Then in the next line are N space separated values of the array.（1<=T<=100；1<=N<=100；-1000<=K<=1000；-100<=A[]<=100）
+
+    Output
+    For each test case in a new line print all the quadruples present in the array separated by space which sums up to value of K.
+    Each quadruple is unique which are separated by a delimeter "$" and are in increasing order.
+     */
 }
--- src/FirstExercise/Problem0115.java
+++ src/FirstExercise/Problem0115.java
@@ -1,4 +1,66 @@
 package FirstExercise;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Problem0115 {
+    /*
+    最小交换次数
+    Description
+    Given an array of N distinct elementsA[ ], find the minimum number of swaps required to sort the array.
+    Your are required to complete the function which returns an integer denoting the minimum number of swaps, required to sort the array.
+
+    Input
+    The first line of input contains an integer T denoting the no of test cases . Then T test cases follow .
+    Each test case contains an integer N denoting the no of element of the array A[ ].
+    In the next line are N space separated values of the array A[ ] .(1<=T<=100;1<=N<=100;1<=A[] <=1000)
+
+    Output
+    For each test case in a new line output will be an integer denoting minimum umber of swaps that are required to sort the array.
+     */
+
+    //获取使得数组升序的最小交换次数
+    public static int getMinSwapCnt(int[] nums) {
+        int[] temp = Arrays.copyOfRange(nums, 0, nums.length);
+        //排序并记录每个数的正确位置
+        Arrays.sort(temp);
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < temp.length; i++) {
+            indexMap.put(temp[i], i);
+        }
+        //记录该位置是否已访问
+        boolean[] isVisited = new boolean[temp.length];
+        //寻找循环节个数，即循环节内的数依次交换，即可全部回到正确位置
+        int loops = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (!isVisited[i]) {
+                int j = i;
+                while (!isVisited[j]) {
+                    isVisited[j] = true;
+                    j = indexMap.get(nums[j]);
+                }
+                loops++;
+            }
+        }
+        //对每一个循环节来说，交换次数为循环节内元素个数减一
+        //举例，若nums存在三个循环节，nums元素个数为n，各循环节元素个数为x, y, z
+        //则交换次数应为 (x-1)+(y-1)+(z-1)=x+y+z-3=n-3
+        //其他情况以此类推，最小交换次数为数组元素个数-循环节个数
+        return nums.length - loops;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N = scanner.nextInt();
+            int[] nums = new int[N];
+            for (int j = 0; j < N; j++) {
+                nums[j] = scanner.nextInt();
+            }
+            System.out.println(getMinSwapCnt(nums));
+        }
+    }
 }
