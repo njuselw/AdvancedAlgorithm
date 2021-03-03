0f4da637d88283b2bc4349d62a5813ef7ba2b74b
njuselw <2463909185@qq.com>
2021-03-03 17:01:44 +0800

    AC 2-2

--- src/SecondExercise/Problem0202.java
+++ src/SecondExercise/Problem0202.java
@@ -1,5 +1,8 @@
 package SecondExercise;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Problem0202 {
@@ -16,11 +19,55 @@ public class Problem0202 {
     输出遍历顺序，用空格隔开
      */
 
+    /**
+     *
+     * @param startIndex 起始点
+     * @param graph 邻接矩阵
+     * @param isVisited 节点访问标记
+     * @param bfsList 遍历结果
+     */
+    public static void bfs(int startIndex, int[][] graph, boolean[] isVisited, List<Integer> bfsList) {
+        if (bfsList.size() >= graph.length) return;
+        if (isVisited[startIndex]) return;
+        //未遍历完，且当前节点未访问
+        isVisited[startIndex] = true;
+        bfsList.add(startIndex);
+        for (int i = 0; i < graph[startIndex].length; i++) {
+            if (graph[startIndex][i] == 1) {
+                bfs(i, graph, isVisited, bfsList);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
+        int T = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < T; i++) {
-
+            String s = scanner.nextLine();
+            int n = Integer.parseInt(s.split(" ")[0]);
+            String startVertex = s.split(" ")[1];
+            String[] vertex_str = scanner.nextLine().split(" ");
+            List<String> vertexes = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                vertexes.add(vertex_str[j]);
+            }
+            Collections.sort(vertexes);
+            int[][] graph = new int[n][n]; //邻接矩阵
+            for (int j = 0; j < n; j++) {
+                String[] strs = scanner.nextLine().split(" ");
+                int index = vertexes.indexOf(strs[0]);
+                for (int k = 0; k < n; k++) {
+                    graph[index][k] = Integer.parseInt(strs[k + 1]);
+                }
+            }
+            List<Integer> bfsList = new ArrayList<>(); //记录广度优先遍历结果
+            boolean[] isVisited  = new boolean[n]; //记录节点访问情况
+            int startIndex = vertexes.indexOf(startVertex);
+            bfs(startIndex, graph, isVisited, bfsList);
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(vertexes.get(bfsList.get(j)) + " ");
+            }
+            System.out.println(vertexes.get(bfsList.get(n - 1)));
         }
     }
 }
