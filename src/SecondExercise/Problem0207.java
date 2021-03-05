154b88ca3678acc2b8f9e5c57299973052331a9c
njuselw <2463909185@qq.com>
2021-03-05 16:09:17 +0800

    AC 2-9, 这个题目描述真的让人难以看懂，绝了

--- src/SecondExercise/Problem0207.java
+++ src/SecondExercise/Problem0207.java
@@ -1,5 +1,7 @@
 package SecondExercise;
 
+import java.util.Scanner;
+
 public class Problem0207 {
     /*
     Searching_3
@@ -24,4 +26,47 @@ public class Problem0207 {
     Output
     For each query output marks obtain by student whose rank is x(1<=x<=10^18).
      */
+
+    public static long getMark(long[] low, long[] len_arr, long x) {
+        int left = 0, right = low.length - 1;
+        //二分方式寻找x所处的区间
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (len_arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        //x所处区间前的所有区间长度和
+        long preLen = left == 0 ? 0 : len_arr[left - 1];
+        return low[left] + x - preLen - 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            int N = scanner.nextInt();
+            int Q = scanner.nextInt();
+            long[] low = new long[N]; //记录各区间的左端点
+            long[] high = new long[N]; //记录各区间的右端点
+            long[] len_arr = new long[N]; //记录各区间的长度
+            long[] query = new long[Q];
+            long len = 0;
+            for (int j = 0; j < N; j++) {
+                low[j] = scanner.nextLong();
+                high[j] = scanner.nextLong();
+                len += high[j] - low[j] + 1;
+                len_arr[j] = len;
+            }
+            for (int j = 0; j < Q; j++) {
+                query[j] = scanner.nextLong();
+            }
+            for (int j = 0; j < Q - 1; j++) {
+                System.out.print(getMark(low, len_arr, query[j]) + " ");
+            }
+            System.out.println(getMark(low, len_arr, query[Q - 1]));
+        }
+    }
 }
