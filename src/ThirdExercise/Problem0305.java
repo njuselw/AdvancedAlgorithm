65fa07ff94fd4ff47b45ad34ced74ddebfc3ef50
njuselw <2463909185@qq.com>
2021-03-13 15:59:15 +0800

    AC 3-5

--- src/ThirdExercise/Problem0305.java
+++ src/ThirdExercise/Problem0305.java
@@ -1,5 +1,10 @@
 package ThirdExercise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Problem0305 {
     /*
     数字重组整除问题
@@ -16,4 +21,48 @@ public class Problem0305 {
     Output
     For each test case in a new line print the desired output.
      */
+
+    public static void permute(List<Integer> list, char[] nums, StringBuilder sb, boolean[] isVisited) {
+        if (sb.length() == nums.length) {
+            list.add(Integer.parseInt(sb.toString()));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!isVisited[i]) {
+                sb.append(nums[i]);
+                isVisited[i] = true;
+                permute(list, nums, sb, isVisited);
+                isVisited[i] = false;
+                sb.deleteCharAt(sb.length() - 1);
+                while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < t; i++) {
+            char[] nums = scanner.nextLine().toCharArray();
+            Arrays.sort(nums);
+            List<Integer> list = new ArrayList<>();
+            boolean[] isVisited = new boolean[nums.length];
+            //全排列
+            permute(list, nums, new StringBuilder(), isVisited);
+            int max = 0;
+            for (Integer num: list) {
+                if (num % 17 == 0 && num > max) {
+                    max = num;
+                }
+            }
+
+            if (max == 0) {
+                System.out.println("Not Possible");
+            } else {
+                System.out.println(max);
+            }
+        }
+    }
 }
