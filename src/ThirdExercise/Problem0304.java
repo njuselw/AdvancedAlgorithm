7e313fe0a08cf25fb76606ef4c111574b286c248
njuselw <2463909185@qq.com>
2021-03-13 15:07:32 +0800

    AC 3-4

--- src/ThirdExercise/Problem0304.java
+++ src/ThirdExercise/Problem0304.java
@@ -1,5 +1,7 @@
 package ThirdExercise;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Problem0304 {
@@ -22,13 +24,44 @@ public class Problem0304 {
     If chain can be formed, then print 1, else print 0.
      */
 
+    public static boolean findCircle(List<String> list, String[] strings, boolean[] isVisited, int index) {
+        list.add(strings[index]);
+        isVisited[index] = true;
+
+        if (list.size() == strings.length) {
+            String first = list.get(0);
+            String last = list.get(list.size() - 1);
+            //成环要求最后一个字符串的末尾字符和第一个字符串的首字符相同
+            return first.charAt(0) == last.charAt(last.length() - 1);
+        }
+
+        char c = strings[index].charAt(strings[index].length() - 1);
+        for (int i = 0; i < strings.length; i++) {
+            String s = strings[i];
+            if (!isVisited[i] && s.charAt(0) == c) {
+                if (findCircle(list, strings, isVisited, i)) {
+                    return true;
+                }
+            }
+        }
+        list.remove(list.size() - 1);
+        isVisited[index] = false;
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(scanner.nextLine());
             String[] strings = scanner.nextLine().split(" ");
-
+            List<String> list = new ArrayList<>();
+            boolean[] isVisited = new boolean[n];
+            if (findCircle(list, strings, isVisited, 0)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
         }
     }
 }
