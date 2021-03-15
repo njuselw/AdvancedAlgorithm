5d59e26844baac4e582357f2b0a320eea7e24334
njuselw <2463909185@qq.com>
2021-03-15 14:24:27 +0800

    AC 3-7

--- src/ThirdExercise/Problem0307.java
+++ src/ThirdExercise/Problem0307.java
@@ -1,5 +1,7 @@
 package ThirdExercise;
 
+import java.util.Scanner;
+
 public class Problem0307 {
     /*
     和最大的连续降序字符
@@ -19,4 +21,48 @@ public class Problem0307 {
     For each test case print the longest string.Case 1:Two strings of maximum length are possible- “CBA” and “RPQ”.
     But he wants the string to be of higher ASCII value therefore, the output is “RPQ”.Case 2:The String of maximum length is “JGDA”.
      */
+
+    public static String getMaxSumSequence(String s) {
+        char[] arr = s.toCharArray();
+        boolean[] flag = new boolean[26];
+        for (char c: arr) {
+            flag[c - 'A'] = true;
+        }
+        int maxLen = 0;
+        String maxSumStr = "";
+        for (int i = 0; i < flag.length; i++) {
+            //若该字符在字符串中，则+1, +2, +3, ..., +26寻找下一个字符，循环构建字符序列
+            if (flag[i]) {
+                int step = 1;
+                while (step + i < flag.length) {
+                    int len = 0;
+                    String temp = "";
+                    for (int j = i; j < flag.length; j += step) {
+                        if (flag[j]) {
+                            len++;
+                            temp = (char) (j + 'A') + temp;
+                        } else {
+                            break;
+                        }
+                    }
+                    //选取更长者，或当两字符串相等时，选取首位ASCII码更大者
+                    if (len > maxLen || (len == maxLen && temp.charAt(0) > maxSumStr.charAt(0))) {
+                        maxLen = len;
+                        maxSumStr = temp;
+                    }
+                    step++;
+                }
+            }
+        }
+        return maxSumStr;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        for (int i = 0; i < t; i++) {
+            String s = scanner.next();
+            System.out.println(getMaxSumSequence(s));
+        }
+    }
 }
