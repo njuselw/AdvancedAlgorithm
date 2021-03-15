e56bcc8066890922551754c4856cedfcac41907c
njuselw <2463909185@qq.com>
2021-03-15 22:46:18 +0800

    AC 3-9

--- src/ThirdExercise/Problem0309.java
+++ src/ThirdExercise/Problem0309.java
@@ -1,5 +1,9 @@
 package ThirdExercise;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Problem0309 {
     /*
     字符串匹配问题
@@ -13,4 +17,65 @@ public class Problem0309 {
     Output
     每一个用例输出一行，每行按照找到的位置先后顺序排列，使用空格隔开。
      */
+
+    public static List<Integer> findPatInTxt(String txt, String pat) {
+        char[] t = txt.toCharArray();
+        char[] p = pat.toCharArray();
+        int[] next = getNextArr(p);
+        List<Integer> indexes = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < txt.length()) {
+            if (j == -1 || t[i] == p[j]) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+            if (j == pat.length()) {
+                indexes.add(i - j);
+                j = next[j];
+            }
+        }
+        return indexes;
+    }
+
+    public static int[] getNextArr(char[] a) {
+        int len = a.length;
+        int[] next = new int[len + 1];
+        next[0] = -1;
+        int k = -1, i = 0;
+        while (i < len) {
+            if (k == -1 || a[k] == a[i]) {
+                i++;
+                k++;
+                if (i != len && a[i] == a[k]) {
+                    next[i] = next[k];
+                } else {
+                    next[i] = k;
+                }
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < t; i++) {
+            String[] s = scanner.nextLine().split(",");
+            String txt = s[0];
+            String pat = s[1];
+            List<Integer> indexes = findPatInTxt(txt, pat);
+            if (indexes.size() == 0) {
+                System.out.println();
+                continue;
+            }
+            for (int j = 0; j < indexes.size() - 1; j++) {
+                System.out.print(indexes.get(j) + " ");
+            }
+            System.out.println(indexes.get(indexes.size() - 1));
+        }
+    }
 }
